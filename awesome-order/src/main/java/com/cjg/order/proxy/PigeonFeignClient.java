ac80403e0c749fb5db72ca40b4cb5d6c8dc75dfb
田鹏浩 <573020950@qq.com>
2021-02-19 16:37:28 +0800

    添加刘乐贤222222

--- awesome-order/src/main/java/com/cjg/order/proxy/PigeonFeignClient.java
+++ awesome-order/src/main/java/com/cjg/order/proxy/PigeonFeignClient.java
@@ -19,6 +19,6 @@ public interface PigeonFeignClient {
      * 发送短信
      * */
     @RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
-    //利乐县
+    //利乐县liulexian
     ResultDTO<MessageSendResponseDTO> sendMessage(@RequestBody MessageSendRequestDTO messageSendRequestDTO);
 }
